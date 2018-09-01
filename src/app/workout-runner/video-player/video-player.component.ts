1567df40e5bb7fb190d2bdb44093caa1e4d18603
Christian Loaiza Garcia <chr.loaiza@gmail.com>
2018-08-31 19:47:36 -0500

    Avance 7 Minutes workout

---
+++ src/app/workout-runner/video-player/video-player.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit, OnChanges, Input } from '@angular/core';
+import { SafeResourceUrl, DomSanitizer } from '@angular/platform-browser';
+
+@Component({
+  selector: 'app-video-player',
+  templateUrl: './video-player.component.html',
+  styles: []
+})
+export class VideoPlayerComponent implements OnInit, OnChanges {
+  private youtubeUrlPrefix = '//www.youtube.com/embed/';
+
+  @Input() videos: Array<string>;
+  safeVideoUrls: Array<SafeResourceUrl>;
+
+  constructor(private sanitizer: DomSanitizer) { }
+
+  ngOnInit() {
+  }
+
+  ngOnChanges() {
+    this.safeVideoUrls = this.videos ? this.videos.map(
+                        v => this.sanitizer.bypassSecurityTrustResourceUrl(this.youtubeUrlPrefix + v))
+                        : this.videos;
+  }
+
+}
